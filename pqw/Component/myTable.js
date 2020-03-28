832a0e7cbeda999b81cc4544bbd088e1549e2e49
PengQiWei <2269718867@qq.com>
2020-03-28 20:03:08 +0800

    彭祺炜2020-03-28

--- pqw/Component/myTable.js
+++ pqw/Component/myTable.js
@@ -1,5 +1,7 @@
 Vue.component('mytable', {
-	template: `<el-table :data="pagelist" width="100%"  border highlight-current-row  @current-change="handleCurrentChange">
+	template: `<el-table :data="pagelist" width="100%" highlight-current-row  @current-change="handleCurrentChange">
+	<slot name="index">
+    </slot>
         <el-table-column v-for="(item,index) in tablehead" :key="index" :label="item.label" :property="item.property" align="center">
               <template slot-scope="scope">{{scope.row[scope.column.property]}}</template>
          </el-table-column>
