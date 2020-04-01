089722efb0ee24dd0e46c3bcd7ef9ca59847b6e9
java-xiaojhu <60861195+java-xiaojhu@users.noreply.github.com>
2020-04-01 21:07:16 +0800

    登录搞定，权限开始

--- js/menu_const.js
+++ js/menu_const.js
@@ -1,9 +1,36 @@
 //页面菜单映射【自行维护】
 var menuMapping = {
-	"g001": "guahao.html",
-	"s001": "huajia.html",
-	"s002": "shoufei.html",
-	"d001": "chufang.html",
-	"o001": "yonghu.html",
-	"o002": "juese.html"
+	//权限
+	"g321": "lyh/Rolelimits.html",
+	"g322": "lyh/Emplimits.html",
+	//维修
+	"a321": "lzj/jieche.html",
+	"a322": "lzj/jungong.html",
+	//商品
+	"c321": "商品资料",
+	"c322": "采购开单",
+	"c323": "入库管理",
+	"c324": "维修领料",
+	"c325": "销售商品",
+	//前台
+	"b31": "sh/jszx.html",
+	"b322": "sh/hyczk.html",
+	//客服
+	"d321": "ckf/che.html",
+	"d322": "ckf/che1.html",
+	//数据
+	"e321": "服务项目",
+	"e322": "车型档案",
+	"e323": "供货单位",
+	"e324": "发动机品牌",
+	//人事
+	"h321": "组织机构",
+	"h322": "pqw/jobDescription.html",
+	"h323": "pqw/exitRegistration.html",
+	"h324": "pqw/addressList.html",
+	"h325": "pqw/mechanicStar.html",
+	"h326": "pqw/classMechanic.html",
+	"h327": "pqw/fieldVehicle.html",
+	//财务
+	"f": ""
 }
--- js/view_filter.js
+++ js/view_filter.js
@@ -1,12 +1,12 @@
 //利用ajax同步验证session【非常重要】
 var rsTxt=$.ajax({
 	type:"get",
-	url:"http://127.0.0.1:8080/api/users/getSessionUser",
+	url:"http://127.0.0.1:8080/lyh/Emps/getSessionUser",
 	async:false
 }).responseText;
 
 if(rsTxt==""){
-	location.href="sessionerror.html";
+	location.href="error.html";
 }
 
 //本地缓存验证session
